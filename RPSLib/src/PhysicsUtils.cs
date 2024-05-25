c9f58f24503e21bea7c1bba76897465287ff5ec1
matthew-vale <matt.vale1@gmail.com>
2024-05-25 18:29:59 +0100

    Updated folder structure

---
+++ RPSLib/src/PhysicsUtils.cs
@@ -0,0 +1,40 @@
+﻿/* Author:		Matthew Vale
+ * Role:        Lead Game Developer
+ * Company:		Red Phoenix Studios
+*/
+using UnityEngine;
+
+namespace RPS {
+
+    public class PhysicsUtils {
+
+        public class Raycast {
+
+            /// <summary>
+            /// Get a reference to the GameObject at the mouse position using a raycast.
+            /// </summary>
+            public static GameObject? GetGameObjectAtMousePosition(float rayDistance, LayerMask maskToCheck, Camera? sourceCamera = null) {
+                Ray ray;
+
+                if (sourceCamera == null) {
+                    if (Camera.main == null) {
+                        Debug.Log("No Camera in scene! Set a Main Camera or pass one into this method.", DebugStyle.CriticalError, true);
+                        return null;
+                    }
+                    ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                } else {
+                    ray = sourceCamera.ScreenPointToRay(Input.mousePosition);
+                }
+
+                if (Physics.Raycast(ray, out RaycastHit rayHit, rayDistance, maskToCheck)) {
+                    return rayHit.transform.gameObject;
+                }
+
+                return null;
+            }
+
+        }
+
+    }
+
+}
---
+++ RPSLib/src/Utils.cs
@@ -0,0 +1,52 @@
+﻿/* Author:		Matthew Vale
+ * Role:		Lead Game Developer
+ * Company:		Red Phoenix Studios
+*/
+using UnityEngine;
+
+namespace RPS {
+
+	public class Utils {
+
+		/// <summary>
+		/// Frame rate and VSync setting.
+		/// </summary>
+		public class FPSUtil {
+
+            public enum VSyncMode { OFF, ON }
+
+            /// <summary>
+            /// Set the Application's target framerate and VSync mode (default ON).
+            /// </summary>
+            public static void SetFramesPerSecond(int targetFPS, VSyncMode vsync = VSyncMode.ON) {
+				Application.targetFrameRate = targetFPS;
+            }
+			
+			/// <summary>
+			/// Set the Application's vertical sync mode.
+			/// </summary>
+			public static void SetVSyncMode(VSyncMode mode) {
+				QualitySettings.vSyncCount = (int)mode;
+            }
+        }
+
+		/// <summary>
+		/// Game time manipulation.
+		/// </summary>
+		public class TimeUtil {
+
+			public static void SetTimeScale(float value) {
+				Time.timeScale = value;
+            }
+
+			public static void LerpTimeScale(float from, float to, float atSpeedOf) {
+				while(Time.timeScale != to) {
+					Time.timeScale = Mathf.MoveTowards(from, to, atSpeedOf) * Time.unscaledDeltaTime;
+				}				
+            }
+
+        }
+
+    }
+
+}
