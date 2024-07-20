fb2449ab8f391733ae6821d80a8bb7f95a9cc4a1
matthew-vale <matt.vale1@gmail.com>
2024-07-20 18:30:59 +0100

    Added weight chance method

--- RPSLib/src/MathsUtils.cs
+++ RPSLib/src/MathsUtils.cs
@@ -50,6 +50,45 @@ namespace RPS {
 
         }
 
+        public class WeightedChances {
+
+            public static int GetRandomWeightedIndex(float[] weights) {
+
+                if (weights == null || weights.Length == 0)
+                    return -1;
+
+                float w;
+                float t = 0;
+                int i; // Use the same float for both loops
+
+                for (i = 0; i < weights.Length; i++) {
+                    w = weights[i];
+
+                    if (float.IsPositiveInfinity(w)) {
+                        return i;
+                    } else if (w >= 0f && !float.IsNaN(w)) {
+                        t += weights[i];
+                    }
+                }
+
+                float r = Random.value;
+                float s = 0f;
+
+                for (i = 0; i < weights.Length; i++) {
+                    w = weights[i];
+                    if (float.IsNaN(w) || w <= 0f)
+                        continue;
+
+                    s += w / t;
+                    if (s >= r)
+                        return i;
+                }
+
+                return -1;
+            }
+
+        }
+
     }
 
 }
