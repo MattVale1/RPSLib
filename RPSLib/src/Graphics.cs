452ed51f0f3ad18a77fe2bd058b6e4554a8b4fe2
matthew-vale <matt.vale1@gmail.com>
2025-05-14 18:39:25 +0100

    Added Graphics and Time classes

---
+++ RPSLib/src/Graphics.cs
@@ -0,0 +1,48 @@
+﻿/// ------------------------------
+/// Original Author: Matthew Vale
+/// ------------------------------
+
+using UnityEngine;
+
+namespace RPSLib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Graphics
+    {
+        /// <summary>
+        /// Display settings such as VSync, FPS and screen resolution.
+        /// </summary>
+        public class Display
+        {
+
+            public enum VSyncMode { OFF, ON }
+
+            /// <summary>
+            /// Set the Application's target framerate and VSync mode (default ON).
+            /// </summary>
+            public static void SetFramesPerSecond(int targetFPS, VSyncMode vsync = VSyncMode.ON)
+            {
+                if (vsync == VSyncMode.ON)
+                {
+                    QualitySettings.vSyncCount = 1;
+                }
+                else
+                {
+                    QualitySettings.vSyncCount = 0;
+                }
+
+                Application.targetFrameRate = targetFPS;
+            }
+
+            /// <summary>
+            /// Set the Application's vertical sync mode.
+            /// </summary>
+            public static void SetVSyncMode(VSyncMode mode)
+            {
+                QualitySettings.vSyncCount = (int)mode;
+            }
+        }
+    }
+}
---
+++ RPSLib/src/Time.cs
@@ -0,0 +1,48 @@
+﻿/// ------------------------------
+/// Original Author: Matthew Vale
+/// ------------------------------
+
+using System;
+using UnityEngine;
+
+namespace RPSLib
+{
+    /// <summary>
+    /// Various helpers for manipulating time.
+    /// </summary>
+    public class Time
+    {
+
+        /// <summary>
+        /// Sets the time scale.
+        /// </summary>
+        [Obsolete("Use Time.timeScale instead of routing the same thing through RPSLib.")]
+        public static void SetTimeScale(float value)
+        {
+            UnityEngine.Time.timeScale = value;
+        }
+
+        // TODO: Test this function and decide if you should remove it.
+        /// <summary>
+        /// Smoothly interpolates towards a target time scale. Run this outside Update functions.
+        /// </summary>
+        [Obsolete("Because this is in a while loop, it might not run separately and hang the thread until done.")]
+        public static void LerpTimeScale(float from, float to, float atSpeedOf)
+        {
+            while (UnityEngine.Time.timeScale != to)
+            {
+                UnityEngine.Time.timeScale = Mathf.MoveTowards(from, to, atSpeedOf) * UnityEngine.Time.unscaledDeltaTime;
+            }
+        }
+
+        // TODO: Remove the "V2" from the method name when we confirm if the original method works.
+        /// <summary>
+        /// Smoothly interpolates towards a target time scale. Run this inside Update function.
+        /// </summary>
+        public static void LerpTimeScaleV2(float from, float to, float atSpeedOf)
+        {
+            UnityEngine.Time.timeScale = Mathf.MoveTowards(from, to, atSpeedOf) * UnityEngine.Time.unscaledDeltaTime;
+        }
+
+    }
+}
